5fe89c9a30a547e45fcf674d95959184ebbd98f4
mel-mouk <mathilde@elmouktafi.fr>
2023-02-08 19:36:53 +0100

    Simple matrix map

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
+
+const TILE_SIZE = 32.0;
 
 void main() {
   runApp(const MyApp());
@@ -19,8 +22,47 @@ class MyApp extends StatelessWidget {
 class SimpleLevel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Bonfire + Meteor POC")),
+    TileModelSprite waterSprite = TileModelSprite(path: "tiles/water.png");
+    TileModelSprite grassSprite = TileModelSprite(path: "tiles/grass.png");
+
+    return BonfireWidget(
+        map: MatrixMapGenerator.generate(
+            axisInverted: true,
+            matrix: [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            builder: (ItemMatrixProperties prop) {
+              TileModelSprite? sprite;
+              if (prop.value == 0) { sprite = waterSprite; }
+              else if (prop.value == 1) { sprite = grassSprite; }
+              else { throw new Exception("Value ${prop.value} doesn't match any sprite"); }
+
+              return TileModel(
+                  x: prop.position.x,
+                  y: prop.position.y,
+                  sprite: sprite,
+                  width: TILE_SIZE,
+                  height: TILE_SIZE,
+              );
+            },
+        )
     );
   }
 }
